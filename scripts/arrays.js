1405af2e577f5278d5c19edc31d73b4ba7f872f8
AidenEyre <EyreAiden@gmail.com>
2020-10-30 15:30:09 -0600

    Added Comment header for the css and js files.
    I will not need to touch the HTML file for this assignment.

---
+++ scripts/arrays.js
@@ -0,0 +1,8 @@
+"use strict";   // Use strict JavaScript rules.
+/*
+   5-5 Challenge: 
+   Using Dates, Arrays, Loops, and Conditional Statements
+
+   Author: Aiden Eyre
+   Date:   30 October 2020
+*/
