11cda77825ae817bac2cef194399cdc0900c662e
AidenEyre <EyreAiden@gmail.com>
2020-10-30 16:50:10 -0600

    Moved some code around for readability and initialized two arrays and a twoArrays() function.

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -11,44 +11,51 @@
 /***************     Part 1: Declare, Initialize and Display Arrays     ***************/
 /**************************************************************************************/
 
-myFamily(); // Call the myfamily function.
+// Initialize arrays.
+let firstNames = [
+  "Sydney",
+  "Matt",
+  "Alyssa",
+  "Raquel",
+  "Shane",
+  "Garet",
+  "Jade",
+  "Lauren",
+  "Drake",
+  "Tristan",
+]; // Array of family first names.
+let nameRelationship = [
+  "Wife",
+  "Father",
+  "Step-Mother",
+  "Mother",
+  "Step-Father",
+  "Brother",
+  "Sister-In-Law",
+  "Sister",
+  "Step-Brother",
+  "Brother",
+]; // Array of relationships of the names in the previous array.
+
+// Call the myfamily function.
+myFamily();
 
 // Display a table of my family to to the web page.
 function myFamily() {
-  let firstNames = [
-    "Sydney",
-    "Matt",
-    "Alyssa",
-    "Raquel",
-    "Shane",
-    "Garet",
-    "Jade",
-    "Lauren",
-    "Drake",
-    "Tristan",
-  ];    // Array of family first names.
-  let nameRelationship = [
-    "Wife",
-    "Father",
-    "Step-Mother",
-    "Mother",
-    "Step-Father",
-    "Brother",
-    "Sister-In-Law",
-    "Sister",
-    "Step-Brother",
-    "Brother",
-  ];    // Array of relationships of the names in the previous array.
-  let myFamilyHTML = "<table id='familyTable'>" +
-                     "<tr><th>Name</th><th>Relationship</th></tr>"; // Initialize html code for the "My Family" table.
-
+  let myFamilyHTML =
+    "<table id='familyTable'>" + "<tr><th>Name</th><th>Relationship</th></tr>"; // Initialize html code for the "My Family" table.
 
   // Loop through firstNames array and add each person to the "My Family" table.
-  for(let i = 0; i < firstNames.length; i++) {
-    myFamilyHTML += "<tr>" +
-                    "<td>" + firstNames[i] + "</td>" +
-                    "<td>" + nameRelationship[i] + "</td>" +
-                    "</tr>";    // Add table row with first name and relationship.
+  for (let i = 0; i < firstNames.length; i++) {
+    myFamilyHTML +=
+      "<tr>" +
+      "<td>" +
+      firstNames[i] +
+      "</td>" +
+      "<td>" +
+      nameRelationship[i] +
+      "</td>" +
+      "</tr>"; // Add table row with first name and relationship.
   }
 
   // Close off myFamilyHTML code and update the family innerHTML with the new string.
@@ -68,76 +75,82 @@ colorArray.unshift("red");
 colorArray.splice(2, 0, "green", "purple", "brown", "yellow", "pink");
 
 // Call functions for each section.
-allColors();            // Call the allColors function.
-colorsWithP();          // Call the colorsWithp function.
-colorsWithoutB();       // Call the colorsWithoutB function.
+allColors(); // Call the allColors function.
+colorsWithP(); // Call the colorsWithp function.
+colorsWithoutB(); // Call the colorsWithoutB function.
 colorsNotContainingN(); // call the colorsNotContainingN function.
-
 
 // Display entire colorArray.
 function allColors() {
-    // Create string that will list each color from the array in the web page.
-    let allColorsHTML = "<ul>"
-    for(let i = 0; i < colorArray.length; i++) {
-        allColorsHTML += "<li>" + colorArray[i] + "</li>";
-    }
-    allColorsHTML += "</ul>";
-    document.getElementById("allColors").innerHTML = allColorsHTML;
+  // Create string that will list each color from the array in the web page.
+  let allColorsHTML = "<ul>";
+  for (let i = 0; i < colorArray.length; i++) {
+    allColorsHTML += "<li>" + colorArray[i] + "</li>";
+  }
+  allColorsHTML += "</ul>";
+  document.getElementById("allColors").innerHTML = allColorsHTML;
 }
-
 
 // Display colors from colorArray that start with p.
 function colorsWithP() {
-    // Create string that will list each color from the array in the web page.
-    let allColorsHTML = "<ul>"
-    for(let i = 0; i < colorArray.length; i++) {
-        // If the 0 index of each color of the array equals p, then add to the string.
-        if(colorArray[i][0] === 'p') {
-            allColorsHTML += "<li>" + colorArray[i] + "</li>";
-        }
+  // Create string that will list each color from the array in the web page.
+  let allColorsHTML = "<ul>";
+  for (let i = 0; i < colorArray.length; i++) {
+    // If the 0 index of each color of the array equals p, then add to the string.
+    if (colorArray[i][0] === "p") {
+      allColorsHTML += "<li>" + colorArray[i] + "</li>";
     }
-    allColorsHTML += "</ul>";
-    document.getElementById("pColors").innerHTML = allColorsHTML;
+  }
+  allColorsHTML += "</ul>";
+  document.getElementById("pColors").innerHTML = allColorsHTML;
 }
-
 
 // Display colors from colorArray that don't start with b.
 function colorsWithoutB() {
-    // Create string that will list each color from the array in the web page.
-    let allColorsHTML = "<ul>"
-    for(let i = 0; i < colorArray.length; i++) {
-        // If the 0 index of each color of the array equals p, then add to the string.
-        if(colorArray[i][0] != 'b') {
-            allColorsHTML += "<li>" + colorArray[i] + "</li>";
-        }
+  // Create string that will list each color from the array in the web page.
+  let allColorsHTML = "<ul>";
+  for (let i = 0; i < colorArray.length; i++) {
+    // If the 0 index of each color of the array equals p, then add to the string.
+    if (colorArray[i][0] != "b") {
+      allColorsHTML += "<li>" + colorArray[i] + "</li>";
     }
-    allColorsHTML += "</ul>";
-    document.getElementById("nonBColors").innerHTML = allColorsHTML;
+  }
+  allColorsHTML += "</ul>";
+  document.getElementById("nonBColors").innerHTML = allColorsHTML;
 }
-
 
 // Display colors that don't contain the letter 'n'.
 function colorsNotContainingN() {
-    // Create string that will list each color from the array in the web page.
-    let allColorsHTML = "<ul>"
-    for(let i = 0; i < colorArray.length; i++) {
-        if(containsLetter(colorArray[i], 'n') === false) {
-            allColorsHTML += "<li>" + colorArray[i] + "</li>";
-        }
+  // Create string that will list each color from the array in the web page.
+  let allColorsHTML = "<ul>";
+  for (let i = 0; i < colorArray.length; i++) {
+    if (containsLetter(colorArray[i], "n") === false) {
+      allColorsHTML += "<li>" + colorArray[i] + "</li>";
     }
-    allColorsHTML += "</ul>";
-    document.getElementById("filterColors").innerHTML = allColorsHTML;
+  }
+  allColorsHTML += "</ul>";
+  document.getElementById("filterColors").innerHTML = allColorsHTML;
 }
-
 
 // Function to check if a string contains a chosen letter.
 function containsLetter(str, letter) {
-    // Loop through string and check if each letter is the chosen letter.
-    for (var i = 0; i < str.length; i++) {
-        if(str[i] === letter) {
-            return true;    // If found, return true.
-        }
-      }
-    return false;   // Return false if nothing is not found.
+  // Loop through string and check if each letter is the chosen letter.
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === letter) {
+      return true; // If found, return true.
+    }
+  }
+  return false; // Return false if nothing is not found.
 }
 
+/**************************************************************************************/
+/***************************     Part 3: Sorting Arrays     ***************************/
+/**************************************************************************************/
+
+// Initialize arrays.
+let animals = ["hippo", "dog", "cat", "donkey", "bird", "whale"];
+let numArray = [51, 14, -2, 128, 46, 88];
+
+function twoArrays() {
+    
+}
