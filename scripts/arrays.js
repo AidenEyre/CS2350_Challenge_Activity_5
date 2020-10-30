f6b2f97b349cf11c54ea6605c762d3ab995bb4c1
AidenEyre <EyreAiden@gmail.com>
2020-10-30 15:37:09 -0600

    Added arrays for family members and their relationships to me.

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -1,4 +1,4 @@
-"use strict";   // Use strict JavaScript rules.
+"use strict"; // Use strict JavaScript rules.
 /*
    5-5 Challenge: 
    Using Dates, Arrays, Loops, and Conditional Statements
@@ -6,3 +6,32 @@
    Author: Aiden Eyre
    Date:   30 October 2020
 */
+
+// Display an array of my family.
+function myFamily() {
+  let firstNames = [
+    "Sydney",
+    "Matt",
+    "Alyssa",
+    "Raquel",
+    "Shane",
+    "Garet",
+    "Jade",
+    "Lauren",
+    "Drake",
+    "Tristan",
+  ];    // Array of family first names.
+
+  let nameRelationship = [
+    "Wife",
+    "Father",
+    "Step-Mother",
+    "Mother",
+    "Step-Father",
+    "Brother",
+    "Sister-In-Law",
+    "Sister",
+    "Step-Brother",
+    "Brother",
+  ];    // Array of relationships of the names in the previous array.
+}
